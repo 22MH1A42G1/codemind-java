d9cf5aea7a3407587da49c348b0ab3ce56e020ff
Aditya <119723334+22MH1A42G1@users.noreply.github.com>
2023-08-04 21:18:35 +0530

    import java.util.Scanner; import java.lang.Math; public class java{     public static void main(String args[]){         Scanner sc = new Scanner(System.in);         int a,b,c,d;         d=sc.nextInt();         a=sc.nextInt();         b=sc.nextInt();         c=3*d-a-b;         System.out.println(c);     } }

---
+++ Average_weight.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.lang.Math;
+public class java{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a,b,c,d;
+        d=sc.nextInt();
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=3*d-a-b;
+        System.out.println(c);
+    }
+}
