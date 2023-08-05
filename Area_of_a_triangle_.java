b4294140bba31c80ff5fb2a71ff21360b6fd20d3
Aditya <119723334+22MH1A42G1@users.noreply.github.com>
2023-08-05 16:06:01 +0530

    import java.util.Scanner; import java.lang.Math; public class java{     public static void main(String args[]){         Scanner sc = new Scanner(System.in);         int a,b,c;         double S,A;         a=sc.nextInt();         b=sc.nextInt();         A=0.5*b*a;         System.out.println((int)A);     } }

---
+++ Area_of_a_triangle_.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.lang.Math;
+public class java{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a,b,c;
+        double S,A;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        A=0.5*b*a;
+        System.out.println((int)A);
+    }
+}
