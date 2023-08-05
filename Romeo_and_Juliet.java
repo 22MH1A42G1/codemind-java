a8445740626450d44f341adb5383171a48b9603c
Aditya <119723334+22MH1A42G1@users.noreply.github.com>
2023-08-05 15:43:44 +0530

    import java.util.Scanner; import java.lang.Math; public class java{     public static void main(String args[]){         Scanner sc = new Scanner(System.in);         int x, y, z;         x = sc.nextInt();         y = sc.nextInt();         z = sc.nextInt();         System.out.println((x*5+y*10)/z);     } }

---
+++ Romeo_and_Juliet.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.lang.Math;
+public class java{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int x, y, z;
+        x = sc.nextInt();
+        y = sc.nextInt();
+        z = sc.nextInt();
+        System.out.println((x*5+y*10)/z);
+    }
+}
