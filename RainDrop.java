05dce5085dc2975db064ab510ee587adfa6f7dfb
Aditya <119723334+22MH1A42G1@users.noreply.github.com>
2023-08-06 22:54:32 +0530

    import java.util.*; public class RainDrop{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int n;         n=sc.nextInt();         if(n%3==0)             System.out.printf("Pling");         if(n%5==0)             System.out.printf("Plang");         if(n%7==0)             System.out.printf("Plong");         if(n%3!=0 && n%5!=0 && n%7!=0)             System.out.println(n);     } }

---
+++ RainDrop.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class RainDrop{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n;
+        n=sc.nextInt();
+        if(n%3==0)
+            System.out.printf("Pling");
+        if(n%5==0)
+            System.out.printf("Plang");
+        if(n%7==0)
+            System.out.printf("Plong");
+        if(n%3!=0 && n%5!=0 && n%7!=0)
+            System.out.println(n);
+    }
+} 
