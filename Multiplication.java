fc8e6b5cf0c4aae08fd94744ce35e34914f8271e
Aditya <119723334+22MH1A42G1@users.noreply.github.com>
2023-08-06 22:07:46 +0530

    import java.util.Scanner; public class Multiplication{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int n,a;         n=sc.nextInt();         a=sc.nextInt();         System.out.println(n*a);     } }

---
+++ Multiplication.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Multiplication{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n,a;
+        n=sc.nextInt();
+        a=sc.nextInt();
+        System.out.println(n*a);
+    }
+}
