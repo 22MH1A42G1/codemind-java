bf5ec9fd16aaef3ab9f932cf79e2421686b418ce
Aditya <119723334+22MH1A42G1@users.noreply.github.com>
2023-08-06 22:27:01 +0530

    import java.util.Scanner; public class diskCapacity{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int a,b,c;         a=sc.nextInt();         b=sc.nextInt();         c=sc.nextInt();         System.out.print(a*b*c*1024);     } }

---
+++ Disk_Capacity.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class diskCapacity{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int a,b,c;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=sc.nextInt();
+        System.out.print(a*b*c*1024);
+    }
+}
