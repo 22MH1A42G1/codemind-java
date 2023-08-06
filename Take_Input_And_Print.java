f0972b77acc240d493aff5eeb2a2bc7ea0b33bb7
Aditya <119723334+22MH1A42G1@users.noreply.github.com>
2023-08-06 22:16:40 +0530

    import java.util.Scanner; public class TIGO{//take input give output to print which are given to the input     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         String n;         n=sc.nextLine();         System.out.println(n);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class TIGO{//take input give output to print which are given to the input
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String n;
+        n=sc.nextLine();
+        System.out.println(n);
+    }
+}
