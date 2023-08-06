5a1fe2aac5c7af32c938f069dcbd15b688ef7817
Aditya <119723334+22MH1A42G1@users.noreply.github.com>
2023-08-06 22:45:56 +0530

    import java.util.*; public class EorO{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int n,a;         n=sc.nextInt();         if(n%2==0)         System.out.print("Even");         else         System.out.print("Odd");     } }

---
+++ Check_if_number_is_even_or_odd.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class EorO{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n,a;
+        n=sc.nextInt();
+        if(n%2==0)
+        System.out.print("Even");
+        else
+        System.out.print("Odd");
+    }
+}
