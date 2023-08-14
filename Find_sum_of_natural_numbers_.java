0bed343c68a905367f605e817c88be455fd856d4
Aditya <119723334+22MH1A42G1@users.noreply.github.com>
2023-08-15 02:47:29 +0530

    import java.util.*; public class SumOfNaturalNumbers {     public static void main(String[] aditya) {         Scanner scanner = new Scanner(System.in);         int N = scanner.nextInt();         System.out.println((N * (N + 1)) / 2);     } }

---
+++ Find_sum_of_natural_numbers_.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class SumOfNaturalNumbers {
+    public static void main(String[] aditya) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        System.out.println((N * (N + 1)) / 2);
+    }
+}
