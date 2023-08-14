bbdef7647c68c0cae307490f5a4a45f8a79c0464
Aditya <119723334+22MH1A42G1@users.noreply.github.com>
2023-08-15 03:49:53 +0530

    import java.util.*; public class aditya {     public static int addDigits(int n){         if(n==0) return 0;         if(n%9==0) return 9;         return n%9;     }     public static void main(String args[]){         Scanner s = new Scanner(System.in);         int a = s.nextInt();         System.out.println(addDigits(a));     } }

---
+++ Add_Digits.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class aditya
+{
+    public static int addDigits(int n){
+        if(n==0) return 0;
+        if(n%9==0) return 9;
+        return n%9;
+    }
+    public static void main(String args[]){
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        System.out.println(addDigits(a));
+    }
+}
