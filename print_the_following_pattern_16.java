444019b9c036db4334a3371f33cfbbcc8ce08592
Aditya <119723334+22MH1A42G1@users.noreply.github.com>
2023-08-15 03:42:13 +0530

    import java.util.*; public class aditya{ public static void main(String args[]){     int a;     Scanner s=new Scanner(System.in);     a=s.nextInt();     for(int i=1;i<=a;i++)     {         for(int j=1;j<=a;j++)         {             System.out.format("%d ",i);
            }
            System.out.println();
        }
    }
    }

---
+++ print_the_following_pattern_16.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class aditya{
+public static void main(String args[]){
+    int a;
+    Scanner s=new Scanner(System.in);
+    a=s.nextInt();
+    for(int i=1;i<=a;i++)
+    {
+        for(int j=1;j<=a;j++)
+        {
+            System.out.format("%d ",i);
+            
+        }
+        System.out.println();
+    }
+}
+}
