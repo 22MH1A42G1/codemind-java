143586e959c2ef6e2b87576210bc75a53655bafe
Aditya <119723334+22MH1A42G1@users.noreply.github.com>
2023-08-15 01:49:23 +0530

    import java.util.*; public class Aditya{     public static void main(String Grades[]){         Scanner sc=new Scanner(System.in);         int a,b,c;         boolean x,y,z;         a=sc.nextInt();         b=sc.nextInt();         c=sc.nextInt();         x=a>50;         y=b>60;         z=c>100;         if(x && y && z){             System.out.print(10);         }         else if(x && y){             System.out.print(9);         }         else if(y && z){             System.out.print(8);         }         else if( x && z){             System.out.print(7);         }         else if(x || y || z){             System.out.print(6);         }         else{             System.out.print(5);         }     } }

--- Grades_of_Rides.java
+++ Grades_of_Rides.java
@@ -1,6 +1,6 @@
 import java.util.*;
-public class Grades{
-    public static void main(String args[]){
+public class Aditya{
+    public static void main(String Grades[]){
         Scanner sc=new Scanner(System.in);
         int a,b,c;
         boolean x,y,z;
@@ -10,17 +10,23 @@ public class Grades{
         x=a>50;
         y=b>60;
         z=c>100;
-        if(x && y && z)
-        System.out.print(10);
-        else if(x && y)
-        System.out.print(9);
-        else if(y && z)
-        System.out.print(8);
-        else if( x && z)
-        System.out.print(7);
-        else if(x || y || z)
-        System.out.print(6);
-        else
-        System.out.print(5);
+        if(x && y && z){
+            System.out.print(10);
+        }
+        else if(x && y){
+            System.out.print(9);
+        }
+        else if(y && z){
+            System.out.print(8);
+        }
+        else if( x && z){
+            System.out.print(7);
+        }
+        else if(x || y || z){
+            System.out.print(6);
+        }
+        else{
+            System.out.print(5);
+        }
     }
 }
