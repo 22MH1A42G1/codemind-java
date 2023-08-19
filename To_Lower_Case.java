ed17c2d178e0fd23885b9fa5f197fc0d0f211179
Aditya <119723334+22MH1A42G1@users.noreply.github.com>
2023-08-19 23:03:08 +0530

    import java.util.Scanner; public class Demo {   public static void main(String[] args) {     Scanner sc = new Scanner (System.in);     String s1 = sc.nextLine();     String s2 = s1.toLowerCase();     System.out.println(s2);   } }

---
+++ To_Lower_Case.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Demo {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner (System.in);  
+    String s1 = sc.nextLine();
+    String s2 = s1.toLowerCase();
+    System.out.println(s2);
+  }
+}
