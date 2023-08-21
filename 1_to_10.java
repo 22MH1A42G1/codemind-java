a933038d9ae48263d2e749586623f7feb6f8ed93
Aditya <119723334+22MH1A42G1@users.noreply.github.com>
2023-08-21 16:06:02 +0530

    import java.util.*; public class aditya{     public static void main(String adi[]){         Scanner s = new Scanner(System.in);         int n=10,i;
            for(i=1;i<=n;i++){
                System.out.print(i+" ");
            }
        }
    }

---
+++ 1_to_10.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class aditya{
+    public static void main(String adi[]){
+        Scanner s = new Scanner(System.in);
+        int n=10,i;
+        
+        for(i=1;i<=n;i++){
+            System.out.print(i+" ");
+        }
+    }
+}
