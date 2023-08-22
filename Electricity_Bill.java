48b905ac94c58cf2f92e1804b7a623691dc1163a
Aditya <119723334+22MH1A42G1@users.noreply.github.com>
2023-08-22 13:51:17 +0530

    import java.util.Scanner;
    public class ElectricityBillCalculator {
        public static void main(String[] $aditya) {
            Scanner scanner = new Scanner(System.in);
            int u;
            float c, b, sr, tb;
            u = scanner.nextInt();
    
            if (u < 200) {
                c = 1.20f;
                b = u * c;
            } else if (u >= 200 && u < 400) {
                c = 1.40f;
                b = u * c;
            } else if (u >= 400 && u < 600) {
                c = 1.60f;
                b = u * c;
            } else if (u >= 600 && u < 800) {
                c = 1.80f;
                b = u * c;
            } else {
                c = 2.00f;
                b = u * c;
            }
    
            if (b > 400) {
                sr = b * 0.15f;
                tb = b + sr;
            } else {
                sr = 0;
                tb = b + sr;
            }
    
            System.out.println("Units Consumed: " + u);
            System.out.println("Cost per Unit: " + String.format("%.2f", c));
            System.out.println("Bill: " + String.format("%.2f", b));
            System.out.println("Surcharge: " + String.format("%.2f", sr));
            System.out.println("Total Amount: " + String.format("%.2f", tb));
            
            scanner.close();
        }
    }

--- Electricity_Bill.java
+++ Electricity_Bill.java
@@ -1,29 +1,43 @@
-import java.util.*;
-public class Aditya{
-    public static void main(String eb[]){
-        double b,u;
-        Scanner sc=new Scanner(System.in);
-        String s1=sc.nextLine();
-        String s2=sc.nextLine();
-        u=sc.nextDouble();
-        if(u<199){
-            b=1.20*u;
+import java.util.Scanner;
+
+public class ElectricityBillCalculator {
+    public static void main(String[] $aditya) {
+        Scanner scanner = new Scanner(System.in);
+        int u;
+        float c, b, sr, tb;
+        u = scanner.nextInt();
+
+        if (u < 200) {
+            c = 1.20f;
+            b = u * c;
+        } else if (u >= 200 && u < 400) {
+            c = 1.40f;
+            b = u * c;
+        } else if (u >= 400 && u < 600) {
+            c = 1.60f;
+            b = u * c;
+        } else if (u >= 600 && u < 800) {
+            c = 1.80f;
+            b = u * c;
+        } else {
+            c = 2.00f;
+            b = u * c;
         }
-        else if(u>=200 && u<400){
-            b=1.5*u;
+
+        if (b > 400) {
+            sr = b * 0.15f;
+            tb = b + sr;
+        } else {
+            sr = 0;
+            tb = b + sr;
         }
-        else if(u>=400 && u<600){
-            b=1.8*u;
-        }
-        else{
-            b=2*u;
-        }
-        if(b>400){
-            b+=(0.15*b);
-        }
-        else{
-            b+=100;
-        }
-        System.out.printf("%.2f",b);
+
+        System.out.println("Units Consumed: " + u);
+        System.out.println("Cost per Unit: " + String.format("%.2f", c));
+        System.out.println("Bill: " + String.format("%.2f", b));
+        System.out.println("Surcharge: " + String.format("%.2f", sr));
+        System.out.println("Total Amount: " + String.format("%.2f", tb));
+        
+        scanner.close();
     }
 }
