3402a6b098db78765aa84780c57536c924b4106d
Aditya <119723334+22MH1A42G1@users.noreply.github.com>
2023-08-24 21:50:46 +0530

    import java.util.Scanner; public class aditya{     public static void main(String args[]){         Scanner sc = new Scanner(System.in);         int p,t,r;         p=sc.nextInt();         t=sc.nextInt();         r=sc.nextInt();         System.out.println((p*t*r)/100);     } }

---
+++ SIMPLE_INTEREST.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class aditya{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int p,t,r;
+        p=sc.nextInt();
+        t=sc.nextInt();
+        r=sc.nextInt();
+        System.out.println((p*t*r)/100);
+    }
+}
