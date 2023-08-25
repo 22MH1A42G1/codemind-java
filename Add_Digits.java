c9f043300e9da78e9e5ac1a8805451b5cc7435aa
Aditya <119723334+22MH1A42G1@users.noreply.github.com>
2023-08-25 21:00:12 +0530

    import java.util.Scanner; public class AddDigits {     public static void main(String[] $aditya) {         Scanner sc = new Scanner(System.in);         int n = sc.nextInt();         int r = addDigits(n);         System.out.println(r);     }
        public static int addDigits(int n) {
            int sum = n;
            while (sum >= 10) {
                String sumStr = Integer.toString(sum);
                int[] digits = new int[sumStr.length()];
                for (int i = 0; i < sumStr.length(); i++) {
                    digits[i] = Character.getNumericValue(sumStr.charAt(i));
                }
                sum = sumOfDigits(digits);
            }
            return sum;
        }
    
        public static int sumOfDigits(int[] digits) {
            int sum = 0;
            for (int digit : digits) {
                sum += digit;
            }
            return sum;
        }
    }

--- Add_Digits.java
+++ Add_Digits.java
@@ -1,14 +1,30 @@
-import java.util.*;
-public class aditya
-{
-    public static int addDigits(int n){
-        if(n==0) return 0;
-        if(n%9==0) return 9;
-        return n%9;
+import java.util.Scanner;
+public class AddDigits {
+    public static void main(String[] $aditya) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int r = addDigits(n);
+        System.out.println(r);
     }
-    public static void main(String args[]){
-        Scanner s = new Scanner(System.in);
-        int a = s.nextInt();
-        System.out.println(addDigits(a));
+
+    public static int addDigits(int n) {
+        int sum = n;
+        while (sum >= 10) {
+            String sumStr = Integer.toString(sum);
+            int[] digits = new int[sumStr.length()];
+            for (int i = 0; i < sumStr.length(); i++) {
+                digits[i] = Character.getNumericValue(sumStr.charAt(i));
+            }
+            sum = sumOfDigits(digits);
+        }
+        return sum;
+    }
+
+    public static int sumOfDigits(int[] digits) {
+        int sum = 0;
+        for (int digit : digits) {
+            sum += digit;
+        }
+        return sum;
     }
 }
