3c9459954526ce16f29395d234a3b53c5368fbbb
Aditya <119723334+22MH1A42G1@users.noreply.github.com>
2023-08-25 11:13:15 +0530

    import java.util.*; public class Sumofthegivennumber{     public static void main(String $aditya[]){         Scanner sc = new Scanner (System.in);         int T = sc.nextInt();         for(int i=0;i<T;i++){             int a=sc.nextInt();             int b=sc.nextInt();             System.out.println(a+b);         }     } }

---
+++ sum_of_the_given_numbers.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Sumofthegivennumber{
+    public static void main(String $aditya[]){
+        Scanner sc = new Scanner (System.in);
+        int T = sc.nextInt();
+        for(int i=0;i<T;i++){
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            System.out.println(a+b);
+        }
+    }
+}
