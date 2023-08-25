b8a9f33d73634144fe070120aa2a4c7d32c0a5ab
Aditya <119723334+22MH1A42G1@users.noreply.github.com>
2023-08-25 20:28:22 +0530

    import java.util.*; public class StrictlyOdd{     public static void main(String arggs[]){         Scanner sc = new Scanner(System.in);         int n = sc.nextInt();         int a[] = new int[n];         int i,c=0,s=0;         for(i=0;i<n;i++){             a[i]=sc.nextInt();         }         for(i=0;i<n;i++){             if(a[i]%2==0){                 c+=1;             }             if(a[i]%2==0 && i%2==0){                 s+=1;             }         }         System.out.println((s==c)?"True":"False");     } }

---
+++ Strictly_EVEN.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class StrictlyOdd{
+    public static void main(String arggs[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a[] = new int[n];
+        int i,c=0,s=0;
+        for(i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+        for(i=0;i<n;i++){
+            if(a[i]%2==0){
+                c+=1;
+            }
+            if(a[i]%2==0 && i%2==0){
+                s+=1;
+            }
+        }
+        System.out.println((s==c)?"True":"False");
+    }
+}
